208aa9e2f912deda9c1e8b14b78537f0951c0e19
Ties <tieslaarakker@gmail.com>
2026-03-11 10:29:43 +0100

    test

--- src/app/[calendar]/layout.tsx
+++ src/app/[calendar]/layout.tsx
@@ -3,17 +3,17 @@ import type { Metadata } from 'next'
 // import { SpeedInsights } from '@vercel/speed-insights/next'
 // import { Analytics } from '@vercel/analytics/react';
 
-import '@/app/globals.css'
+// import '@/app/globals.css'
 
 import Menu from '@/components/header/Menu'
 // import ModButton from '@/components/header/ModButton'
 // import metaPages from '@/components/header/metadata'
 // import TopMenu from '@/components/header/TopMenu';
 // import Header from '@/components/header/Header';
-import { notFound } from 'next/navigation';
-import { headers } from 'next/headers';
-import { canonical, metaData } from '@/lib/supabase'
-import { getHostName } from '@/lib/paths'
+// import { notFound } from 'next/navigation';
+// import { headers } from 'next/headers';
+// import { canonical, metaData } from '@/lib/supabase'
+// import { getHostName } from '@/lib/paths'
 
 // export async function generateMetadata(
 //   { params }: { params: Promise<{ calendar: string }> }
@@ -31,8 +31,8 @@ export default async function RootLayout({
   params: Promise<{ calendar: string }>
 }) {
 
-  const calendar = (await params).calendar  
-  const host = getHostName(await headers())
+  // const calendar = (await params).calendar  
+  // const host = getHostName(await headers())
 
   // if (metaData.get(calendar)?.canonical !== canonical.get(host)) notFound()
 
